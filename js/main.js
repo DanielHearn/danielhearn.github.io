f3e611ebfea2f4656506b5eb1c0452ab50e48a3b
DanielHearn <hearndaniel@live.co.uk>
2017-07-09 17:41:02 +0100

    Fix main js animation triggering

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 function nav() {
   var button = document.getElementById("overlay-button");
-  var header = document.getElementById("overlay-header");
   var overlayButtons = document.getElementsByClassName("overlay-li");
   document.getElementById("overlay").classList.toggle("active");
   document.getElementById("top").classList.toggle("active");
   button.classList.toggle("active");
   button.classList.toggle("fixed");
 
-  header.classList.toggle("fadeIn");
-  header.classList.toggle("animated");
    for(i = 0; i < overlayButtons.length; i++){
-     overlayButtons[i].classList.toggle("fadeInUp");
+     overlayButtons[i].classList.toggle("fadeIn");
      overlayButtons[i].classList.toggle("animated");
     }
 };
