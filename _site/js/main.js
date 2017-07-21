dbed89e6ff639150077d720727189823fa6f599d
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-07-21 12:48:15 +0100

    Change overlay ul css animations

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,13 +1,18 @@
 function nav() {
   var button = document.getElementById("overlay-button");
-  var overlayButtons = document.getElementsByClassName("overlay-li");
+  //var overlayButtons = document.getElementsByClassName("overlay-li");
+  var overlayUl = document.getElementById("overlay-ul");
+
   document.getElementById("overlay").classList.toggle("active");
   document.getElementById("top").classList.toggle("active");
   button.classList.toggle("active");
   button.classList.toggle("fixed");
 
-   for(i = 0; i < overlayButtons.length; i++){
+  overlayUl.classList.toggle("fadeIn");
+  overlayUl.classList.toggle("animated");
+
+   /*for(i = 0; i < overlayButtons.length; i++){
      overlayButtons[i].classList.toggle("fadeIn");
      overlayButtons[i].classList.toggle("animated");
-    }
+   }*/
 };
