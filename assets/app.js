14ef9d8e9211ab209b4a4a4eded0fc98e5186165
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-08-10 17:34:23 +0100

    Remove particle js logging

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,2 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+particlesJS.load('particles-js', 'assets/particles.json', function() {});
