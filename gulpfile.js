c5a53129f047dbe09bf8bf52b423c397b6e13c03
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-10-27 17:10:36 +0100

    Add initial files for new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
 var runSequence = require('run-sequence');
 var cache = require('gulp-cache');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
 var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
+var ghpages = require('gh-pages');
+var path = require('path');
 
-const scssSource = 'assets/styles/*.scss';
-const cssDest = 'assets/styles/';
+const scssSource = 'src/scss/*.scss';
+const cssDest = 'src/css';
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
 
 gulp.task('sass', function() {
   return gulp.src(scssSource)
@@ -17,31 +32,80 @@ gulp.task('sass', function() {
     .pipe(cssnano({zindex: false}))
 })
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
+gulp.task('useref', function(){
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    //.pipe(gulpIf('src/js/*.js', gulp.dest('dist')))
+    //.pipe(gulpIf('*.css', cssnano({zindex: false})))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function (callback){
+gulp.task('js', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('css', function(){
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('src/img/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('favicons', function(){
+  return gulp.src('src/favicons/*.+(png|jpg|jpeg|gif|svg|ico|xml|json)')
+  .pipe(gulpIf('*.+(png|jpg|jpeg|gif|svg)', cache(imagemin())))
+  .pipe(gulp.dest('dist/favicons'))
+});
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('src/pug/*.pug')
+    .pipe(pug().on('error', function(err) {
+      console.log(err);
+    }))
+    .pipe(gulp.dest("src/"))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*']);
+});
+
+gulp.task('CNAME', function(){
+  return gulp.src('CNAME')
+  .pipe(gulp.dest('dist/'))
+});
+
+
+gulp.task('watch', ['browserSync', 'pug', 'sass'], function (callback){
+  gulp.watch('src/pug/*.pug', ['pug']);
   gulp.watch(scssSource, ['sass']);
-  gulp.watch('*.html').on('change', browserSync.reload)
-  gulp.watch('js/*.js', browserSync.reload)
-  /*runSequence(['pug'], 'watch-files',
-    callback
-  )*/
+  gulp.watch('src/*.html').on('change', browserSync.reload)
+  gulp.watch('src/js/*.js', browserSync.reload)
 })
 
-gulp.task('watch-files', function(callback) {
-  gulp.watch(scssSource, ['sass']);
-  gulp.watch('*.html').on('change', browserSync.reload)
-  gulp.watch('js/*.js', browserSync.reload)
-})
-
-gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['pug', 'sass', 'images', 'favicons', 'js', 'css', 'CNAME'], 'useref',
     callback
   )
 })
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'pug'], 'watch',
+    callback
+  )
+})
+
+gulp.task('deploytopages', function() {
+  ghpages.publish('dist', function(err) {});
+});
+
+gulp.task('deploy', function(callback) {
+  runSequence(['build'], 'deploytopages',
+    callback
+  )
+});
