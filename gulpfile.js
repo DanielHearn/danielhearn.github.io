661cd840dab9ee3903542ce26f266b9cfd5b22ba
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-01-01 14:29:10 +0000

    Add webpack and format gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var useref = require('gulp-useref');
-var runSequence = require('run-sequence');
-var cache = require('gulp-cache');
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var uglify = require('gulp-uglify');
-var cssnano = require('gulp-cssnano');
-var gulpIf = require('gulp-if');
-var pug = require('gulp-pug');
-var browserSync = require('browser-sync').create();
-var ghpages = require('gh-pages');
-var path = require('path');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var useref = require('gulp-useref')
+var runSequence = require('run-sequence')
+var cache = require('gulp-cache')
+var del = require('del')
+var imagemin = require('gulp-imagemin')
+var uglify = require('gulp-uglify')
+var cssnano = require('gulp-cssnano')
+var gulpIf = require('gulp-if')
+var pug = require('gulp-pug')
+var browserSync = require('browser-sync').create()
+var ghpages = require('gh-pages')
+var path = require('path')
 
-const scssSource = 'src/scss/*.scss';
+const scssSource = 'src/scss/*.scss'
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: 'src'
-    },
+    }
   })
-});
+})
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src(scssSource)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css'))
@@ -32,58 +32,57 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream())
 })
 
-gulp.task('useref', function(){
+gulp.task('useref', function () {
   return gulp.src('src/*.html')
     .pipe(useref())
-    //.pipe(gulpIf('src/js/*.js', gulp.dest('dist')))
-    //.pipe(gulpIf('*.css', cssnano({zindex: false})))
+    // .pipe(gulpIf('src/js/*.js', gulp.dest('dist')))
+    // .pipe(gulpIf('*.css', cssnano({zindex: false})))
     .pipe(gulp.dest(''))
-});
+})
 
-gulp.task('js', function(){
+gulp.task('js', function () {
   return gulp.src('src/js/*.js')
     .pipe(gulp.dest('js'))
-});
+})
 
-gulp.task('css', function(){
+gulp.task('css', function () {
   return gulp.src('src/css/*.css')
     .pipe(gulp.dest('css'))
     .pipe(gulp.dest('src/css'))
-});
+})
 
-gulp.task('images', function(){
+gulp.task('images', function () {
   return gulp.src('src/img/*.+(png|jpg|jpeg|gif|svg)')
     .pipe(cache(imagemin()))
     .pipe(gulp.dest('img'))
-});
+})
 
-gulp.task('assets', function(){
+gulp.task('assets', function () {
   return gulp.src('src/assets/*')
     .pipe(gulp.dest('assets'))
-});
+})
 
-gulp.task('favicons', function(){
+gulp.task('favicons', function () {
   return gulp.src('src/favicons/*.+(png|jpg|jpeg|gif|svg|ico|xml|json|webmanifest)')
-  .pipe(gulpIf('*.+(png|jpg|jpeg|gif|svg)', cache(imagemin())))
-  .pipe(gulp.dest('favicons'))
-});
+    .pipe(gulpIf('*.+(png|jpg|jpeg|gif|svg)', cache(imagemin())))
+    .pipe(gulp.dest('favicons'))
+})
 
-gulp.task('pug', function buildHTML() {
+gulp.task('pug', function buildHTML () {
   return gulp.src('src/pug/*.pug')
-    .pipe(pug().on('error', function(err) {
-      console.log(err);
+    .pipe(pug().on('error', function (err) {
+      console.log(err)
     }))
-    .pipe(gulp.dest("src/"))
-});
+    .pipe(gulp.dest('src/'))
+})
 
+gulp.task('clean:dist', function () {
+  return del.sync(['dist/**/*'])
+})
 
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*']);
-});
-
-gulp.task('watch', ['browserSync', 'pug', 'sass'], function (callback){
-  gulp.watch('src/pug/*.pug', ['pug']);
-  gulp.watch(scssSource, ['sass']);
+gulp.task('watch', ['browserSync', 'pug', 'sass'], function (callback) {
+  gulp.watch('src/pug/*.pug', ['pug'])
+  gulp.watch(scssSource, ['sass'])
   gulp.watch('src/*.html').on('change', browserSync.reload)
   gulp.watch('src/js/*.js', browserSync.reload)
 })
@@ -95,18 +94,8 @@ gulp.task('build', function (callback) {
   )
 })
 
-gulp.task('default', function(callback) {
+gulp.task('default', function (callback) {
   runSequence(['sass', 'pug'], 'watch',
     callback
   )
 })
-/*
-gulp.task('deploytopages', function() {
-  ghpages.publish('dist', function(err) {});
-});
-
-gulp.task('deploy', function(callback) {
-  runSequence(['build'], 'deploytopages',
-    callback
-  )
-});*/
