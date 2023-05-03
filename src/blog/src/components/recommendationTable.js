ca3a1285328220a81456cabe3bc18262fdb6200f
Daniel Hearn <DanielHearn@users.noreply.github.com>
2023-05-03 19:56:33 +0100

    Add pros and cons styling

--- src/blog/src/components/recommendationTable.js
+++ src/blog/src/components/recommendationTable.js
@@ -3,7 +3,7 @@ import * as React from "react"
 const RecommendationTable = ({ pros, cons }) => {
   return (
     <div className="RecommendationTable">
-      <div className="RecommendationTable__column">
+      <div className="RecommendationTable__column RecommendationTable__column--left">
         <div className="RecommendationTable__header">Pros</div>
         <ul className="RecommendationTable__content">
           {pros.map(pro => (
@@ -11,8 +11,8 @@ const RecommendationTable = ({ pros, cons }) => {
           ))}
         </ul>
       </div>
-      <div className="RecommendationTable__column">
-        <div className="RecommendationTable__header">Con</div>
+      <div className="RecommendationTable__column RecommendationTable__column--right">
+        <div className="RecommendationTable__header">Cons</div>
         <ul className="RecommendationTable__content">
           {cons.map(con => (
             <li>- {con}</li>
